0f0552b61fc6c26edd37e49e8431924b8301f998
Mikhail <3lou4yballl@gmail.com>
2023-09-02 14:46:02 +0300

    Добавлен фильтр для администраторов

--- app/telegram_bot/src/filters/authorization_filter.py
+++ app/telegram_bot/src/filters/authorization_filter.py
@@ -10,5 +10,14 @@ class AuthorisationFilter(Filter):
         filter_result = message.from_user.id in self.white_list
         # print('Filter result: ', filter_result)
         if not filter_result:
-            await message.answer('Нет доступа')
+            await message.answer('Нет доступа, необходима авторизация')
         return filter_result
+
+class AdministratorFilter(Filter):
+    def __init__(self) -> None:
+        self.admin_list = [6280571470, ]
+
+    async def __call__(self, message: types.Message) -> bool:
+        filter_result = message.from_user.id in self.admin_list
+        if not filter_result:
+            await message.answer('Нет доступа, данная команда доступна только администратору.')
