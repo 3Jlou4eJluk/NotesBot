160a776e1ec81062b1db877633665f63ba37224c
Mikhail <3lou4yballl@gmail.com>
2023-09-02 15:18:46 +0300

    Настроен bot_setttings.py: теперь он подтягивает ml_api_link из переменной окружения NOTES_BOT_ML_API_LINK

--- app/telegram_bot/src/bot_settings.py
+++ app/telegram_bot/src/bot_settings.py
@@ -1,4 +1,8 @@
-ml_api_link = 'http://ml_server:8000/ml_api'
+import os
+# переехало в переменную окружения NOTES_BOT_ML_API_LINK
+# ml_api_link = 'http://ml_server:8000/ml_api'
+
+ml_api_link = os.getenv('NOTES_BOT_ML_API_LINK')
 
 # notes to be shown on one inline keyboard page
 note_page_count = 4
