3c8e203e0bb358189111a938f7ec0cbfa7f88cf3
Mikhail <3lou4yballl@gmail.com>
2023-09-02 15:17:36 +0300

    Настроен скрипт run.sh для запуска без построения docker container

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
 cd ..
-sudo docker-compose up
+source app/telegram_bot/src/telegram_bot_venv/bin/activate
+export NOTES_BOT_ML_API_LINK = "127.0.0.1:8000/ml_api"
+python3 app/telegram_bot/src/telegram_bot.py &
+
+
+source app/ml_server/src/ml_server_venv/bin/activate
+uvicorn app/ml_server/src/ml_server:ml_api --host 127.0.0.1 --port 8000
